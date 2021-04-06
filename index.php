d7f44f712a9c6979d296cd16e44ec41069ee6c1d
Johannes Ringe <Ringe.Johannes@mh-hannover.de>
2021-04-06 14:59:26 +0200

    Update

--- index.php
+++ index.php
@@ -14,21 +14,26 @@ if(isset($_SERVER['HTTP_REFERER']))
     echo "<!--" . $_SERVER['HTTP_REFERER'] . "-->";
 
 */
-$content_folder = "./content";  // content folder
-$request = "hello-world";       // init page, keep empty for the last post
+$content_folder = "./content/dynamic";  // content folder
+$static_folder = "./content/static";
+$request = "hello_world";       // init page, keep empty for the last post
 $title = "d@tenpir.at";
-$theme = "haxx0r";
+$theme = "dark2";
 
 $content = "";
 $id = 0;
 $is_welcome_page = false;
 $is_first_visit = true;
+$active_category = "";
 
 if(isset($_SERVER['HTTP_REFERER']))
     $is_first_visit = !preg_match("/https?:\/\/" . $_SERVER['HTTP_HOST'] . "/i",$_SERVER['HTTP_REFERER']);
 
+$static_pages = list_files($static_folder,".md");
+
 $pages = list_files($content_folder,".md");
-$static_pages = list_files($content_folder . "/static/",".md");
+
+//echo "<!-- \$pages\n";var_dump($pages);echo "-->\n";
 
 if(isset($_GET['theme']) && !empty($_GET['theme'])){
     $tmp_theme = preg_replace('/[^A-Z\-a-z0-9_]+/', "", $_GET['theme']);
@@ -47,19 +52,20 @@ if(!empty($request))
 if($id !== false){  // dynamic page found!
     $page = $pages[array_keys($pages)[$id]];
     $file = $page['file'];
+    $active_category = $page['category'];
     $title = htmlentities($page['basename']);
-    $content = parse_md_file("$content_folder/$file");
+    $content = parse_md_file($file,$pages);
 }
-else{   // dynamic page not found
+else{   // no dynamic page found
     $static_id = find_entry($static_pages,$request);
     if($static_id !== false){  // static page found!
         $page = $static_pages[array_keys($static_pages)[$static_id]];
         $file = $page['file'];
-        $content = parse_md_file("$content_folder/static/$file");
+        $content = parse_md_file($file,$pages);
     }
-    else{
+    else{ // nothing found
         http_response_code(404);
-        $content = parse_md_file($content_folder . "/static/404.md");
+        $content = parse_md_file("$static_folder/404.md",$pages);
     }
 }
 ?>
@@ -77,54 +83,122 @@ else{   // dynamic page not found
         <a rel="keep-params" href="/">d<span class="accent">@</span>tenpir<span class="accent">.</span>at</a></div>
     </div>
 <?php 
-    foreach ($pages as $key => $value){
-        $text = $value["basename"];
-        $link = $value["link"];
-        $time = $value["time"];
-        echo "\t\t<a rel=\"keep-params\" " . ($request == $link ? "class=\"active\"" : "") . " href=\"/$link\">$text</a>\n";
-        echo "\t\t<span class=\"nav_time\">" . date ("d.m.Y H:i", $time) . "</span>\n";
-    }
+        $cats = array_unique(array_column($pages, 'category'));
+        sort($cats);
+        foreach($cats as $cat)
+            display_category($cat,$pages,$request,$active_category);
 ?>
-    <div class="static">
+<div class="static">
 <?php 
         echo "\t\t<a rel=\"keep-params\" " . ($request == "datenschutz" ? "class=\"active\"" : "") . "href=\"datenschutz\">Datenschutz</a>\n";
         echo "\t\t<a rel=\"keep-params\" " . ($request == "impressum" ? "class=\"active\"" : "") . "href=\"impressum\">Impressum</a>\n";
 ?>
-    </div>
 </div>
+</div>
+
+
+
 <div class="content">
 <?php echo $content; ?>
 </div>
 </body>
 </html>
 <?php
-function list_files($path, $filter){
-    $dir = opendir($path);
-    $list = array();
-    while($file = readdir($dir)){
-        if(!starts_with($file,'.') && ends_with($file,$filter)){
-            $basename = basename($file, $filter);
-            $cache_file = "$path/cache/" . basename($file). ".cache";
-            $link = format_link($basename);
-            $time = filemtime("$path/$file");
 
-            if(file_exists($cache_file))
+function display_category($category,$pages,$request,$active_category){
+    $p = get_in_array($category,$pages,"category");
+    if(!empty($category))
+        echo "\t\t<span class=\"category\">" . highlight($category,1) . "</span>\n";
+    echo "\t\t<div class=\"" . ($active_category == $category ? " expanded" : "") ."\" id=\"$category\">\n";
+    foreach($p as $key => $value)
+        display_entry($value,($request == $value["link"]));
+    echo "\t\t</div>\n";
+    
+}
+
+function display_entry($value,$active = false){
+    $text = $value["basename"];
+    $link = $value["link"];
+    $time = $value["time"];
+    $category = $value["category"];
+    echo "\t\t\t<div class=\"menu_link" . ($active ? " active" : " inactive") . "\">\n";
+    echo "\t\t\t\t<a rel=\"keep-params\" class=\"\" href=\"/$link\">" . highlight_sonderzeichen($text) . "</a>\n";
+    echo "\t\t\t\t<span class=\"nav_time\">" . date ("d.m.Y H:i", $time) . "</span>\n";
+    echo "\t\t\t</div>\n";
+}
+
+function highlight_random($text){
+    $r = rand(0,strlen($text)-1);
+    if($r == 0)
+        return "<span class=\"accent\">" . substr($text, $r, 1) . "</span>". substr($text, $r+1);
+    else
+        return substr($text, 0, $r) . "<span class=\"accent\">" . substr($text, $r, 1) . "</span>". substr($text, $r+1);
+}
+
+function highlight_sonderzeichen($text){
+    return preg_replace('/([^a-z0-9 ])/i', "<span class=\"accent\">$1</span>", $text);
+}
+
+function highlight($text,$r){
+    return substr($text, 0, $r) . "<span class=\"accent\">" . substr($text, $r, 1) . "</span>". substr($text, $r+1);
+}
+
+function get_in_array( string $needle, array $haystack, string $column ){
+    return array_filter( $haystack, function( $item )use( $needle, $column ){
+      return $item[ $column ] === $needle;
+    });
+}
+
+function list_files($dir, $filter, $basedir = null, &$results = array()) {
+
+    $files = scandir($dir);
+
+    foreach ($files as $key => $file) {
+        $path = $dir . DIRECTORY_SEPARATOR . $file;
+        if (!is_dir($path) && ends_with($file,$filter)) {       // File
+
+            $basename = basename($file, $filter);
+            $cache_file = "./cache/" . basename($file). ".cache";
+            $category = basename(dirname(trim_start($path, $basedir == null ? $dir : $basedir)));
+
+            $link = format_link((empty($category) ? "" : "$category-") . $basename);
+
+            $time = filemtime($path);
+            $cache = "";
+            $name = $basename;
+
+            if(file_exists($cache_file)){
                 $time = filemtime($cache_file);
-            
-            $list[$time . ',' . $link] = array(
-                "file"=>$file,
-                "basename"=>$basename,
-                "link"=>$link,
-                "time"=>$time
+                $cache = $cache_file;
+            }
+
+            $filename_file = dirname($path) . DIRECTORY_SEPARATOR . basename($file,$filter) . ".name";
+            if(file_exists($filename_file)){
+                $name = fgets(fopen($filename_file, 'r'));  // read first line
+                
+            }
+
+            $results[$time . ',' . $link] = array(         // hinzufügen
+                "category"  => $category,
+                "file"      => realpath($path),
+                "basename"  => $name,
+                "link"      => $link,
+                "cache"      => $cache,
+                "time"      => $time
             );
+
+            krsort($results);
+
+        } 
+        else if (is_dir($path) && !starts_with($file,'.')) {          // Folder
+            list_files($path, $filter, $basedir == null ? $dir : $basedir, $results);
+            //echo "Wat? $file";
+        }
+        else{
+            // ignore
         }
     }
-    closedir($dir);
-    krsort($list);
-    echo "<!--\n";
-    var_dump($list);
-    echo "-->\n";
-    return $list;
+    return $results;
 }
 
 function starts_with( $haystack, $needle ) {
@@ -132,6 +206,11 @@ function starts_with( $haystack, $needle ) {
 }
 function ends_with( $haystack, $needle ){
     return substr($haystack, -strlen( $needle )) === $needle;
+}
+function trim_start( $str, $prefix ){
+    if (substr($str, 0, strlen($prefix)) == $prefix)
+        return substr($str, strlen($prefix));
+    return $str;
 }
 function format_link( $s ){
     $result = preg_replace("/\s+/", "-", strtolower($s));
@@ -141,13 +220,13 @@ function format_link( $s ){
 function find_entry($haystack, $needle ){
     return array_search( $needle, array_column($haystack, "link"));
 }
-function parse_md_file($file){
+function parse_md_file($file, $pages){
     $text = file_get_contents($file);
     if(preg_match("/^\[(github|softlink)\]\((.*)\)$/",$text, $matches, PREG_UNMATCHED_AS_NULL)) {
         $type = $matches[1];
         $url = $matches[2];
         $basename = basename($file);
-        $cache_file = "./content/cache/$basename.cache" ;
+        $cache_file = "./cache/$basename.cache" ;
         if(file_exists($cache_file)) {
             if(time() - filemtime($cache_file) > 86400) {
                 // too old , re-fetch
@@ -171,21 +250,19 @@ function parse_md_file($file){
 
         $text = $cache;
     }
-    return Parsedown::instance()
+    $result = Parsedown::instance()
         ->setMarkupEscaped(true) # safe mode
         ->setBreaksEnabled(true) # enables automatic line breaks
         ->text($text); 
+    return $result;
 }
 
 function get_last_commit_time_from_github_api($url){
-    
     preg_match("/^(.*:\/\/).*?\/(?<user>.*?)\/(?<repo>.*?)\/(?<branch>.*?)\/(?<path>.*?)$/",$url,$m);
-    //var_dump($m);
     $user = $m["user"];
     $repo = $m["repo"];
     $path = $m["path"];
     $branch = $m["branch"];
-
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_USERAGENT, "datenpir.at-browser"); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -198,7 +275,5 @@ function get_last_commit_time_from_github_api($url){
     $last_commit = $commits[0]["commit"]["committer"]["date"];
     return DateTime::createFromFormat('Y-m-d\TH:i:s+', $last_commit)->format('U');
 }
-
-
 
 ?>
